99ac5dc3c2d446e1c0cd0c9d8d3a41e6564aa74b
lvalderr <83881803+lvalderr@users.noreply.github.com>
2021-08-29 12:44:54 -0700

    Created Repository and added files

---
+++ Main.py
@@ -0,0 +1,82 @@
+# ---------------------------------------------------------------------------------------------------------------------#
+# Title: Main Module
+# Description: A module for testing
+# Change Log: (Who, When, What)
+# LValderrama, 8.27.2021, created Main Module
+# LValderrama, 8.28.2021, added imported processing and IO modules, while loop.
+# ---------------------------------------------------------------------------------------------------------------------#
+
+# Objective:
+# 1. The Assignment09.py script is designed to:
+#       a. Present a menu of choices for the user to select from.
+#       b. Execute the program based on the choice made by the user.
+#       c. Use Try/Catch Error Handling.
+#       d. Use Class objects.
+#       e. Work with other modules
+
+# Pseudo-Code:
+# Data --------------------------------------------------------------------------------------------------------------- #
+# Step 1: Declare variables
+
+strChoice = ""  # Captures the user option selection
+strFileName = 'EmployeeData.txt'  # The name of the data file
+lstTable = []  # A list that acts as a 'table' of rows
+
+# Step 2: Import Module ---------------------------------------------------------------------------------------------- #
+
+if __name__ == "__main__":
+    from DataClasses import Employee as Emp
+    from ProcessingClasses import FileProcessor as Fp
+    from IOClasses import EmployeeIO as Eio
+else:
+    raise Exception("There was an error while importing the scripts")
+
+# Main Body of Script  ----------------------------------------------------------------------------------------------- #
+# Step 3: Load data from file into a list of employee objects when script starts ------------------------------------- #
+
+lstFileData = Fp.read_data_from_file(strFileName)
+lstTable.clear()
+for line in lstFileData:
+    lstTable.append(Emp(line[0], line[1], line[2].strip()))
+
+# Step 4: Calling the functions from DataClasses, IOClasses, ProcessingClasses --------------------------------------- #
+while True:
+    try:
+        # Show user a menu of options -------------------------------------------------------------------------------- #
+        Eio.print_menu_items()
+
+        # Get user's menu option choice ------------------------------------------------------------------------------ #
+        strChoice = Eio.input_menu_options()
+
+        # Show user current data in the list of employee objects ----------------------------------------------------- #
+        if strChoice == '1':
+            Eio.print_current_list_items(lstTable)
+            continue
+
+        # Let user add data to the list of employee objects ---------------------------------------------------------- #
+        elif strChoice == '2':
+            newEmployee = Eio.input_employee_data()
+            lstTable.append(newEmployee)
+            Eio.print_current_list_items(lstTable)
+            continue
+
+        # Let user save current data to file ------------------------------------------------------------------------- #
+        elif strChoice == '3':
+            print('\n Would you like to save your data?')
+            strSaveToFileInput = input("Enter 'y' or 'n': ")
+            if strSaveToFileInput == 'n':
+                print('Data not saved!')
+            if strSaveToFileInput == 'y':
+                Fp.save_data_to_file(strFileName, lstTable)
+                print('\nYour data is saved to', strFileName)
+            continue
+
+        # Let user exit program -------------------------------------------------------------------------------------- #
+        elif strChoice == '4':
+            print("Goodbye!")
+            EndProgram = input('\n(Press Enter to End Program)')
+            break
+        else:
+            print('\nInvalid entry. Please enter a number from 1 to 4')
+    except ValueError:
+        print('\nInvalid entry. Please enter a number from 1 to 4')
---
+++ TestHarness.py
@@ -0,0 +1,90 @@
+# ---------------------------------------------------------------------------------------------------------------------#
+# Title: Test Harness
+# Description: A module for testing
+# Change Log: (Who, When, What)
+# LValderrama, 8.24.2021, Created File
+# LValderrama, 8.26.2021, changed the file name to import and tested the script
+# ---------------------------------------------------------------------------------------------------------------------#
+
+
+# Test IO classes
+if __name__ == "__main__":
+    from DataClasses import Employee as Emp
+    from ProcessingClasses import FileProcessor as Fp
+    from IOClasses import EmployeeIO as Eio
+else:
+    raise Exception("This file was not created to be imported")
+
+# Test data module
+objP1 = Emp(1, "Bob", "Smith")
+objP2 = Emp(2, "Sue", "Jones")
+lstTable = [objP1, objP2]
+for row in lstTable:
+    print(row.to_string(), type(row))
+
+# Test processing module
+Fp.save_data_to_file("EmployeeData.txt", lstTable)
+lstFileData = Fp.read_data_from_file("EmployeeData.txt")
+lstTable.clear()
+for line in lstFileData:
+    lstTable.append(Emp(line[0], line[1], line[2].strip()))
+for row in lstTable:
+    print(row.to_string(), type(row))
+
+# Test IO classes
+Eio.print_menu_items()
+Eio.print_current_list_items(lstTable)
+print(Eio.input_employee_data())
+print(Eio.input_menu_options())
+
+
+class Person(object):  # Inherits from object
+    """Stores data about a persons:
+
+    properties:
+        first_name: (string) with the person's first name
+
+        last_name: (string) with the person's last name
+    methods:
+        to_string() returns comma separated product data (alias for __str__())
+    changelog: (When,Who,What)
+        RRoot,1.1.2030,Created Class
+    """
+
+    # -- Constructor --
+    def __init__(self, first_name, last_name):
+        # -- Attributes --
+        self.__first_name = first_name
+        self.__last_name = last_name
+
+    # -- Properties --
+    @property
+    def first_name(self):
+        return str(self.__first_name).title()
+
+    @first_name.setter
+    def first_name(self, value):
+        if not str(value).isnumeric():
+            self.__first_name = value
+        else:
+            raise Exception("Names cannot be numbers")
+
+    @property
+    def last_name(self):
+        return str(self.__last_name).title()
+
+    @last_name.setter
+    def last_name(self, value):
+        if not str(value).isnumeric():
+            self.__last_name = value
+        else:
+            raise Exception("Names cannot be numbers")
+
+    # -- Methods --
+    def to_string(self):
+        """ Explicitly returns a string with this object's data """
+        return self.__str__()
+
+    def __str__(self):
+        """ Implicitly returns a string with this object's data """
+        return self.first_name + ',' + self.last_name
